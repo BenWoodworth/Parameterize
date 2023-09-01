e0743a92b75d97dec0f5ad628eb810c893e3d8e5
Ben Woodworth <ben@benwoodworth.com>
2023-09-01 17:58:44 -0400

    Factor out common `iterations` code in ParameterizeSpec

--- src/commonTest/kotlin/ParameterizeSpec.kt
+++ src/commonTest/kotlin/ParameterizeSpec.kt
@@ -3,18 +3,38 @@ package com.benwoodworth.parameterize
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 class ParameterizeSpec {
-    @Test
-    fun with_zero_parameters_it_should_iterate_once() {
-        var count = 0
+    /**
+     * Run parameterize with the given block, and assert that each iteration
+     * completes correctly based on the value returned by the [block]. The
+     * [expectedIterations] are the expected values returned each iteration, or
+     * `null` for skipped iterations (when a parameter has no arguments).
+     */
+    private fun <T : Any> testParameterize(
+        expectedIterations: Iterable<T?>,
+        block: ParameterizeScope.() -> T
+    ) {
+        val iterations = mutableListOf<T?>()
 
         parameterize {
-            count++
+            try {
+                iterations += block()
+            } catch (caught: Throwable) {
+                iterations += null
+                throw caught
+            }
         }
 
-        assertEquals(1, count)
+        assertEquals(expectedIterations.toList(), iterations, "Incorrect iterations")
+    }
+
+
+    @Test
+    fun with_zero_parameters_it_should_iterate_once() = testParameterize(
+        listOf("only iteration")
+    ) {
+        "only iteration"
     }
 
     @Test
@@ -54,218 +74,165 @@ class ParameterizeSpec {
     }
 
     @Test
-    fun with_one_parameter_it_should_iterate_once_per_argument() {
-        val letters = ('a'..'z').toList()
+    fun with_one_parameter_it_should_iterate_once_per_argument() = testParameterize('a'..'z') {
+        val letter by parameter('a'..'z')
 
-        val iterations = mutableListOf<Char>()
-        parameterize {
-            val letter by parameter { letters }
-
-            iterations += letter
-        }
-
-        assertEquals(letters, iterations)
+        letter
     }
 
     @Test
-    fun with_two_independent_parameters_it_should_iterate_the_same_as_nested_loops() {
-        val iterations = mutableListOf<String>()
-        parameterize {
-            val letter by parameterOf('a', 'b', 'c', 'd')
-            val number by parameterOf(0, 1, 2, 3)
-
-            iterations += "$letter$number"
-        }
-
-        val expectedIterations = mutableListOf<String>()
-        for (letter in listOf('a', 'b', 'c', 'd')) {
-            for (number in listOf(0, 1, 2, 3)) {
-                expectedIterations += "$letter$number"
-            }
-        }
-
-        assertEquals(expectedIterations, iterations)
-    }
-
-    @Test
-    fun with_two_dependent_parameters_it_should_iterate_the_same_as_nested_loops() {
-        val numbers = (0..4).toList()
-
-        val iterations = mutableListOf<String>()
-        parameterize {
-            val number by parameter { numbers }
-            val differentNumber by parameter { numbers.filter { it != number } }
-
-            iterations += "$number$differentNumber"
-        }
-
-        val expectedIterations = mutableListOf<String>()
-        for (number in numbers) {
-            for (differentNumber in numbers.filter { it != number }) {
-                expectedIterations += "$number$differentNumber"
-            }
-        }
-
-        assertEquals(expectedIterations, iterations)
-    }
-
-    @Test
-    fun parameters_in_a_loop_should_be_independent() {
-        val iterations = mutableListOf<String>()
-        parameterize {
-            var string = ""
-
-            repeat(3) {
-                val letter by parameterOf('a', 'b', 'c')
-                string += letter
-            }
-
-            iterations += string
-        }
-
-        val expectedIterations = mutableListOf<String>()
-        for (letter1 in listOf('a', 'b', 'c')) {
-            for (letter2 in listOf('a', 'b', 'c')) {
-                for (letter3 in listOf('a', 'b', 'c')) {
-                    expectedIterations += "$letter1$letter2$letter3"
+    fun with_two_independent_parameters_it_should_iterate_the_same_as_nested_loops() = testParameterize(
+        buildList {
+            for (letter in listOf('a', 'b', 'c', 'd')) {
+                for (number in listOf(0, 1, 2, 3)) {
+                    add("$letter$number")
                 }
             }
         }
+    ) {
+        val letter by parameterOf('a', 'b', 'c', 'd')
+        val number by parameterOf(0, 1, 2, 3)
 
-        assertEquals(expectedIterations, iterations)
+        "$letter$number"
     }
 
     @Test
-    fun parameter_with_no_arguments_should_finish_iteration_early() {
-        val iterations = mutableListOf<String>()
-
-        parameterize {
-            val digit1 by parameter(1..5)
-            val digit2 by parameter((1..5).filter { it > digit1 })
-            val digit3 by parameter((1..5).filter { it > digit2 })
-
-            val increasingDigits = "$digit1$digit2$digit3"
-            iterations += increasingDigits
-        }
-
-        val expectedIterations = listOf("123", "124", "125", "134", "135", "145", "234", "235", "245", "345")
-        assertEquals(expectedIterations, iterations)
-    }
-
-    @Test
-    fun unused_parameter_with_no_arguments_should_not_finish_iteration_early() {
-        var finishedIteration = false
-
-        parameterize {
-            val unused: String by parameter(emptyList())
-
-            finishedIteration = true
-        }
-
-        assertTrue(finishedIteration)
-    }
-
-    @Test
-    fun parameter_that_depends_on_a_later_one() {
-        val iterations = mutableListOf<String>()
-
-        parameterize {
-            lateinit var laterValue: String
-
-            val earlier by parameter {
-                listOf("${laterValue}1", "${laterValue}2", "${laterValue}3")
+    fun with_two_dependent_parameters_it_should_iterate_the_same_as_nested_loops() = testParameterize(
+        buildList {
+            for (number in 0..4) {
+                for (differentNumber in (0..4).filter { it != number }) {
+                    add("$number$differentNumber")
+                }
             }
-
-            val later by parameterOf("a", "b")
-            laterValue = later
-
-            iterations += earlier
         }
+    ) {
+        val number by parameter(0..4)
+        val differentNumber by parameter { (0..4).filter { it != number } }
 
-        val expectedIterations = listOf("a1", "a2", "a3", "b1", "b2", "b3")
-        assertEquals(expectedIterations, iterations)
+        "$number$differentNumber"
     }
 
     @Test
-    fun parameters_that_are_read_out_of_order() {
-        val iterations = mutableListOf<String>()
-
-        parameterize {
-            val first by parameterOf(1, 2)
-            val second by parameterOf("a", "b")
-
-            iterations += "$second$first"
-        }
-
-        val expectedIterations = listOf("a1", "a2", "b1", "b2")
-        assertEquals(expectedIterations, iterations)
-    }
-
-    @Test
-    fun swapping_unused_parameters() {
-        val iterations = mutableListOf<String>()
-
-        parameterize {
-            val firstReadParameter by parameterOf("a", "b")
-            if (firstReadParameter == "a") {
-                val unread1A: String by parameterOf()
-            } else {
-                val unread1B: String by parameterOf()
+    fun parameters_in_a_loop_should_be_independent() = testParameterize(
+        buildList {
+            for (letter1 in listOf('a', 'b', 'c')) {
+                for (letter2 in listOf('a', 'b', 'c')) {
+                    for (letter3 in listOf('a', 'b', 'c')) {
+                        add("$letter1$letter2$letter3")
+                    }
+                }
             }
+        }
+    ) {
+        var string = ""
 
-            val lastReadParameter by parameterOf("1", "2")
-            if (lastReadParameter == "1") {
-                val unread2A: String by parameterOf()
-            } else {
-                val unread2B: String by parameterOf()
-            }
-
-            iterations += "$firstReadParameter$lastReadParameter"
+        repeat(3) {
+            val letter by parameterOf('a', 'b', 'c')
+            string += letter
         }
 
-        val expectedIterations = listOf("a1", "a2", "b1", "b2")
-        assertEquals(expectedIterations, iterations)
+        string
     }
 
     @Test
-    fun parameter_only_read_from_another_lazy_initialization() {
-        val iterations = mutableListOf<String>()
+    fun parameter_with_no_arguments_should_finish_iteration_early() = testParameterize(
+        listOf("123", "124", "125", "134", "135", "145", null, "234", "235", "245", null, "345", null, null, null)
+    ) {
+        // increasing digits
+        val digit1 by parameter(1..5)
+        val digit2 by parameter((1..5).filter { it > digit1 })
+        val digit3 by parameter((1..5).filter { it > digit2 })
 
-        parameterize {
+        "$digit1$digit2$digit3"
+    }
+
+    @Test
+    fun unused_parameter_with_no_arguments_should_not_finish_iteration_early() = testParameterize(
+        listOf("finished")
+    ) {
+        val unused: String by parameter(emptyList())
+
+        "finished"
+    }
+
+    @Test
+    fun parameter_that_depends_on_a_later_one() = testParameterize(
+        listOf("a1", "a2", "a3", "b1", "b2", "b3")
+    ) {
+        lateinit var laterValue: String
+
+        val earlier by parameter {
+            listOf("${laterValue}1", "${laterValue}2", "${laterValue}3")
+        }
+
+        val later by parameterOf("a", "b")
+        laterValue = later
+
+        earlier
+    }
+
+    @Test
+    fun parameters_that_are_read_out_of_order() = testParameterize(
+        listOf("a1", "a2", "b1", "b2")
+    ) {
+        val first by parameterOf(1, 2)
+        val second by parameterOf("a", "b")
+
+        "$second$first"
+    }
+
+    @Test
+    fun swapping_unused_parameters() = testParameterize(
+        listOf("a1", "a2", "b1", "b2")
+    ) {
+        val firstReadParameter by parameterOf("a", "b")
+        if (firstReadParameter == "a") {
+            val unread1A: String by parameterOf()
+        } else {
+            val unread1B: String by parameterOf()
+        }
+
+        val lastReadParameter by parameterOf("1", "2")
+        if (lastReadParameter == "1") {
+            val unread2A: String by parameterOf()
+        } else {
+            val unread2B: String by parameterOf()
+        }
+
+        "$firstReadParameter$lastReadParameter"
+    }
+
+    @Test
+    fun parameter_only_read_from_another_lazy_initialization() = testParameterize(
+        listOf("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3")
+    ) {
+        val letter by parameter('a'..'c')
+
+        val letterNumber by parameter {
+            (1..3).map { "$letter$it" }
+        }
+
+        letterNumber
+    }
+
+    @Test
+    fun declaring_parameters_within_another_initialization() = testParameterize(
+        listOf("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3")
+    ) {
+        val letterNumber by parameter {
             val letter by parameter('a'..'c')
+            val number by parameter(1..3)
 
-            val letterNumber by parameter {
-                (1..3).map { "$letter$it" }
-            }
-
-            iterations += letterNumber
+            listOf("$letter$number")
         }
 
-        val expectedIterations = listOf("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3")
-        assertEquals(expectedIterations, iterations)
+        letterNumber
     }
 
     @Test
-    fun declaring_parameters_within_another_initialization() {
-        val iterations = mutableListOf<String>()
-
-        parameterize {
-            val letterNumber by parameter {
-                val letter by parameter('a'..'c')
-                val number by parameter(1..3)
-
-                listOf("$letter$number")
-            }
-
-            iterations += letterNumber
-        }
-
-        val expectedIterations = listOf("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3")
-        assertEquals(expectedIterations, iterations)
-    }
-
-    @Test
-    fun custom_lazy_arguments_implementation() {
+    fun custom_lazy_arguments_implementation() = testParameterize(
+        listOf("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3")
+    ) {
         fun <T> ParameterizeScope.customLazyParameter(
             lazyArguments: () -> Iterable<T>
         ): Parameter<T> {
@@ -278,20 +245,13 @@ class ParameterizeSpec {
             return parameter(CustomLazyArguments())
         }
 
-        val iterations = mutableListOf<String>()
+        val letterNumber by customLazyParameter {
+            val letter by parameter('a'..'c')
+            val number by parameter(1..3)
 
-        parameterize {
-            val letterNumber by customLazyParameter {
-                val letter by parameter('a'..'c')
-                val number by parameter(1..3)
-
-                listOf("$letter$number")
-            }
-
-            iterations += letterNumber
+            listOf("$letter$number")
         }
 
-        val expectedIterations = listOf("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3")
-        assertEquals(expectedIterations, iterations)
+        letterNumber
     }
 }
