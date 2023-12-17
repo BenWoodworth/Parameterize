0b084d3bbe4354e97b116fbeb900070dd259f149
Ben Woodworth <ben@benwoodworth.com>
2023-12-17 18:59:22 -0500

    Expose the `parameterize` overload that takes a configuration
    Useful for library users to be able to call this directly with a pre-built configuration, instead of building a new one every call.
    
    Notably for specialized `parameterize`-wrapping functions that will have their own configuration that won't be changed, like a parameterless `parameterizeTest` function that would record failures and hook into the test lifecycle with decorators.

--- src/commonTest/kotlin/ParameterizeConfigurationSpec.kt
+++ src/commonTest/kotlin/ParameterizeConfigurationSpec.kt
@@ -159,6 +159,12 @@ class ParameterizeConfigurationSpec {
                     block
                 )
             }
+        },
+        "with a provided configuration" to {
+            test { configure, block ->
+                val configuration = ParameterizeConfiguration { configure() }
+                parameterize(configuration, block)
+            }
         }
     )
 
