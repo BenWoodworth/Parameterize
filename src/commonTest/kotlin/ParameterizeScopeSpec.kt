4ef208e4946a74b7a8364246340ebc523e2a4e36
Ben Woodworth <ben@benwoodworth.com>
2024-01-02 00:37:05 -0500

    Move `parameter()` tests to the `ParameterizeScopeSpec`
    These functions are more related to the DSL/syntax than the actual `parameterize` behavior, so move them keeps things more organized, especially since more `parameter` overloads will be added later.

--- src/commonTest/kotlin/ParameterizeScopeSpec.kt
+++ src/commonTest/kotlin/ParameterizeScopeSpec.kt
@@ -2,11 +2,64 @@ package com.benwoodworth.parameterize
 
 import com.benwoodworth.parameterize.ParameterizeScope.ParameterDelegate
 import kotlin.properties.PropertyDelegateProvider
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertSame
+import kotlin.test.*
 
+/**
+ * Specifies the [parameterize] DSL and its syntax.
+ */
 class ParameterizeScopeSpec {
+    @Test
+    fun parameter_from_iterable_should_be_constructed_with_the_same_arguments_instance() = parameterize {
+        val iterable = emptyList<Nothing>()
+        val parameter = parameter(iterable)
+
+        assertSame(iterable, parameter.arguments)
+    }
+
+    @Test
+    fun parameter_of_listed_arguments_should_have_the_correct_arguments() = parameterize {
+        data class UniqueClass(val argument: String)
+
+        val listedArguments = listOf(
+            UniqueClass("A"),
+            UniqueClass("B"),
+            UniqueClass("C")
+        )
+
+        val parameter = parameterOf(*listedArguments.toTypedArray())
+
+        assertContentEquals(listedArguments.asIterable(), parameter.arguments)
+    }
+
+    @Test
+    fun parameter_from_lazy_arguments_should_have_the_correct_arguments() = parameterize {
+        val lazyParameter = parameter { 'a'..'z' }
+
+        assertEquals(('a'..'z').toList(), lazyParameter.arguments.toList())
+    }
+
+    @Test
+    fun parameter_from_lazy_arguments_should_not_be_computed_before_declaring() = parameterize {
+        /*val undeclared by*/ parameter<Nothing> { fail("computed") }
+    }
+
+    @Test
+    fun parameter_from_lazy_arguments_should_only_be_computed_once() = parameterize {
+        var evaluationCount = 0
+
+        val lazyParameter = parameter {
+            evaluationCount++
+            1..10
+        }
+
+        repeat(5) { i ->
+            val arguments = lazyParameter.arguments.toList()
+            assertEquals((1..10).toList(), arguments, "Iteration #$i")
+        }
+
+        assertEquals(1, evaluationCount)
+    }
+
     @Test
     fun string_representation_should_show_used_parameter_arguments_in_declaration_order() = parameterize {
         val a by parameterOf(1)
