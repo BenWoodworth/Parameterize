8f9f079e17320ecf8949f292f247915abab1e02d
Ben Woodworth <ben@benwoodworth.com>
2024-01-22 23:38:54 -0500

    Change Parameterize license to Apache 2.0
    Originally the GPL 3.0 license was decided on so that any improvements or derivative projects that benefit from this project would also have to their source be shared and open.
    
    However, GPL imposes itself even if this library is only being used as a dependency. LGPL would've been more in line with the original intent for the license, since it more so applies to modification of the project/binaries, and doesn't impose itself on other projects when used as a dependency. But even so, though, there's concern about licensing under any of the GPL-based licenses because it's widely considered to be high risk in professional settings. The potential for litigation if accidentally misused means the GPL licenses won't be as likely to be whitelisted for use in corporate projects, and that's concerning for Parameterize's adoption.
    
    The Apache 2.0 license was chosen instead because of this concern since it's more permissive, and common on license whitelists because of it being low risk. It was chosen over other permissive licenses (e.g. the MIT license) as it seems to be the license of choice in the Kotlin ecosystem, being used by all the first-party Kotlin libraries, and a comfortable majority overall.

--- buildSrc/src/main/kotlin/parameterize.library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/parameterize.library-conventions.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Ben Woodworth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 val ciVersion: String? = System.getenv("CI_VERSION")
 
 val ossrhUsername: String? = System.getenv("OSSRH_USERNAME")
@@ -43,8 +59,8 @@ publishing {
 
             licenses {
                 license {
-                    name = "GNU General Public License version 3"
-                    url = "https://opensource.org/licenses/GPL-3.0"
+                    name = "The Apache Software License, Version 2.0"
+                    url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
                 }
             }
             developers {
