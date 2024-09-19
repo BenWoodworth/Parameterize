d604a178e191fc1c0e731e9aaeb77ee3a83f58b9
Ben Woodworth <ben@benwoodworth.com>
2024-09-18 21:57:44 -0400

    Document `ParameterizeScope` and its members
    Now that the interface can be implemented by 3rd parties, knowing what
    the members are for and how it could be used is important to document.

--- src/commonMain/kotlin/ParameterizeScope.kt
+++ src/commonMain/kotlin/ParameterizeScope.kt
@@ -22,29 +22,105 @@ import kotlin.jvm.JvmInline
 import kotlin.jvm.JvmName
 import kotlin.reflect.KProperty
 
-/** @see parameterize */
+/**
+ * The scope for parameterized code blocks, providing extensions to create and declare [Parameter]s.
+ *
+ * With this interface it's possible to introspect arguments and their usage, or even modify the behavior of parameters.
+ * Having direct access to the values, even the arguments themselves can be specially handled or transformed.
+ *
+ * As an example, a testing framework could take advantage of this to run a specific set of arguments and track each of
+ * their values:
+ *
+ * ```
+ * private class PickingParameterizeScope(
+ *     private val parameterizeScope: ParameterizeScope,
+ *     private val pickArgumentIndices: List<Int>
+ * ) : ParameterizeScope by parameterizeScope {
+ *     private var parameterIndex = 0
+ *
+ *     // Intercept the parameter, and declare it with just the one picked argument
+ *     override fun <T> Parameter<T>.provideDelegate(thisRef: Any?, property: KProperty<*>): DeclaredParameter<T> {
+ *         val pickArgumentIndex = pickArgumentIndices.getOrNull(parameterIndex++)
+ *             ?: throw IllegalArgumentException("Argument index not specified for ${property.name}")
+ *
+ *         val pickedArgument = arguments.drop(pickArgumentIndex).take(1).singleOrNull()
+ *             ?: throw IllegalArgumentException("Parameter ${property.name} has no argument at index $pickArgumentIndex")
+ *
+ *         return parameterizeScope.run {
+ *             parameterOf(pickedArgument).provideDelegate(thisRef, property)
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * It might also want to report the values that were used. For that, something like this is also possible:
+ * ```
+ * private class TrackingParameterizeScope(
+ *     private val parameterizeScope: ParameterizeScope,
+ * ) : ParameterizeScope by parameterizeScope {
+ *     private val usedParameters = mutableSetOf<DeclaredParameter<*>>()
+ *
+ *     // Track the parameters that have been used, that way it's possible to report only the relevant values
+ *     override fun <T> DeclaredParameter<T>.getValue(thisRef: Any?, property: KProperty<*>): T {
+ *         usedParameters += this
+ *         return parameterizeScope.run { getValue(thisRef, property) }
+ *     }
+ * }
+ * ```
+ *
+ * @see Parameter
+ */
 @ParameterizeDsl
 public interface ParameterizeScope {
-    /** @suppress */
+    /**
+     * Declares this [Parameter], allowing one of its arguments to be used as the [value][getValue] of the Kotlin
+     * [property].
+     *
+     * @throws ParameterizeException if this [Parameter] is being declared with the wrong [property].
+     * @see Parameter
+     */
     public operator fun <T> Parameter<T>.provideDelegate(thisRef: Any?, property: KProperty<*>): DeclaredParameter<T>
 
-    /** @suppress */
+    /**
+     * Returns the argument that this parameter was [declared][provideDelegate] with.
+     *
+     * @see Parameter
+     */
     public operator fun <T> DeclaredParameter<T>.getValue(thisRef: Any?, property: KProperty<*>): T
 
     /**
+     * A sequence of [arguments] that can be [declared][provideDelegate] within a [ParameterizeScope] to make one of the
+     * [values][getValue] available.
+     *
+     * Parameter builders are available as extensions on [ParameterizeScope], making them straightforward to
+     * [declare][provideDelegate] given some specified/listed/computed arguments:
+     *
+     * ```
+     * val letter by parameter('a'..'z')
+     * val primeUnder20 by parameterOf(2, 3, 5, 7, 11, 13, 17, 19)
+     * val computedValue by parameter { lazilyComputedValues() }
+     * ```
+     *
      * @constructor
      * **Experimental:** Prefer using the scope-limited [parameter] function, if possible.
      * The constructor will be made `@PublishedApi internal` once
      * [context parameters](https://github.com/Kotlin/KEEP/issues/367) are introduced to the language.
-     *
-     * @suppress
      */
     @JvmInline
     public value class Parameter<out T> @ExperimentalParameterizeApi constructor(
+        /**
+         * The [value][getValue]s that this parameter can be [declared][provideDelegate] with.
+         */
         public val arguments: Sequence<T>
     )
 
-    /** @suppress */
+    // KT-72022
+    /**
+     * A [Parameter][Parameter] [declared][provideDelegate] with one of its [arguments][Parameter.arguments] so that it can be used
+     * as the [value][getValue] of a Kotlin property.
+     *
+     * @see Parameter
+     */
     public class DeclaredParameter<out T> internal constructor(
         internal val parameterState: ParameterState,
         internal val argument: T
@@ -75,11 +151,13 @@ public interface ParameterizeScope {
 }
 
 /**
- * Declare a parameter with the given [arguments].
+ * Used to [declare][ParameterizeScope.provideDelegate] a parameter with the supplied [arguments].
  *
  * ```
  * val letter by parameter('a'..'z')
  * ```
+ *
+ * @see Parameter
  */
 @Suppress("UnusedReceiverParameter") // Should only be accessible within parameterize scopes
 @OptIn(ExperimentalParameterizeApi::class)
@@ -87,27 +165,31 @@ public fun <T> ParameterizeScope.parameter(arguments: Sequence<T>): Parameter<T>
     Parameter(arguments)
 
 /**
- * Declare a parameter with the given [arguments].
+ * Used to [declare][ParameterizeScope.provideDelegate] a parameter with the supplied [arguments].
  *
  * ```
  * val letter by parameter('a'..'z')
  * ```
+ *
+ * @see Parameter
  */
 public fun <T> ParameterizeScope.parameter(arguments: Iterable<T>): Parameter<T> =
     parameter(arguments.asSequence())
 
 /**
- * Declare a parameter with the given [arguments].
+ * Used to [declare][ParameterizeScope.provideDelegate] a parameter with the listed [arguments].
  *
  * ```
  * val primeUnder20 by parameterOf(2, 3, 5, 7, 11, 13, 17, 19)
  * ```
+ *
+ * @see Parameter
  */
 public fun <T> ParameterizeScope.parameterOf(vararg arguments: T): Parameter<T> =
     parameter(arguments.asSequence())
 
 /**
- * Declares a parameter with the given [lazyArguments].
+ * Used to [declare][ParameterizeScope.provideDelegate] a parameter with the computed [lazyArguments].
  * The arguments are only computed the first time the parameter is used, and not at all if used.
  *
  * This `parameter` function is useful to avoid computing the arguments every iteration.
@@ -125,6 +207,8 @@ public fun <T> ParameterizeScope.parameterOf(vararg arguments: T): Parameter<T> 
  *
  * - The [lazyArguments] block should not have side effects. Since it's not run every iteration, side effects could make
  *   the execution different from future iterations, breaking [parameterize]'s determinism assumption.
+ *
+ * @see Parameter
  */
 @OptIn(ExperimentalTypeInference::class)
 @OverloadResolutionByLambdaReturnType
@@ -148,7 +232,7 @@ public inline fun <T> ParameterizeScope.parameter(
     })
 
 /**
- * Declares a parameter with the given [lazyArguments].
+ * Used to [declare][ParameterizeScope.provideDelegate] a parameter with the computed [lazyArguments].
  * The arguments are only computed the first time the parameter is used, and not at all if used.
  *
  * This `parameter` function is useful to avoid computing the arguments every iteration.
@@ -166,6 +250,8 @@ public inline fun <T> ParameterizeScope.parameter(
  *
  * - The [lazyArguments] block should not have side effects. Since it's not run every iteration, side effects could make
  *   the execution different from future iterations, breaking [parameterize]'s determinism assumption.
+ *
+ * @see Parameter
  */
 @OptIn(ExperimentalTypeInference::class)
 @OverloadResolutionByLambdaReturnType
