a74586a5595f6fef6686c81877179f9bfc473805
Ben Woodworth <ben@benwoodworth.com>
2024-09-23 15:30:22 -0400

    Break up build script into convention plugins
    The library will be split up into modules, so this makes parts of the
    build configuration easy to reuse.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,96 +14,22 @@
  * limitations under the License.
  */
 
-import kotlinx.validation.ExperimentalBCVApi
-import org.gradle.kotlin.dsl.support.uppercaseFirstChar
 import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.KotlinTargetWithTests
-import java.net.URL
 
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.dokka)
-    alias(libs.plugins.binary.compatibility.validator)
-    id("parameterize.library-conventions")
+    id("kotlin-multiplatform-conventions")
+    id("dokka-conventions")
+    id("binary-compatibility-validator-conventions")
+    id("publishing-conventions")
+    id("ci-conventions")
 }
 
 repositories {
     mavenCentral()
 }
 
-apiValidation {
-    @OptIn(ExperimentalBCVApi::class)
-    klib {
-        enabled = true
-    }
-}
-
-@OptIn(ExperimentalWasmDsl::class)
 kotlin {
-    explicitApi()
-
-    jvm {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_1_8
-        }
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
-    js {
-        browser()
-        nodejs()
-    }
-
-    linuxX64()
-    linuxArm64()
-    androidNativeArm32()
-    androidNativeArm64()
-    androidNativeX86()
-    androidNativeX64()
-    macosX64()
-    macosArm64()
-    iosSimulatorArm64()
-    iosX64()
-    watchosSimulatorArm64()
-    watchosX64()
-    watchosArm32()
-    watchosArm64()
-    tvosSimulatorArm64()
-    tvosX64()
-    tvosArm64()
-    iosArm64()
-    watchosDeviceArm64()
-    mingwX64()
-
-    wasmJs {
-        browser()
-        nodejs()
-    }
-    wasmWasi {
-        nodejs()
-    }
-
-    @OptIn(ExperimentalKotlinGradlePluginApi::class)
-    compilerOptions {
-        freeCompilerArgs.add("-Xexpect-actual-classes")
-    }
-
     sourceSets {
-        all {
-            languageSettings.optIn("kotlin.contracts.ExperimentalContracts")
-        }
-
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
         val jvmMain by getting {
             dependencies {
                 implementation(libs.opentest4j)
@@ -112,79 +38,7 @@ kotlin {
     }
 }
 
-val ciHostTargets = run {
-    val hostTargetPrefixes = mapOf(
-        "linux" to listOf("metadata", "jvm", "js", "linux", "android", "wasm"),
-        "macos" to listOf("macos", "ios", "watchos", "tvos"),
-        "windows" to listOf("mingw")
-    )
-
-    val hostTargets = hostTargetPrefixes.mapValues { (_, prefixes) ->
-        kotlin.targets.filter { target ->
-            prefixes.any { target.name.startsWith(it) }
-        }
-    }
-
-    val envCiHost = System.getenv("CI_HOST")
-    val osName = System.getProperty("os.name")
-    val host = when {
-        envCiHost != null -> envCiHost.also {
-            require(envCiHost in hostTargets) { "Invalid CI_HOST: $envCiHost. Must be one of: ${hostTargets.keys}" }
-        }
-
-        osName == "Linux" -> "linux"
-        osName == "Mac OS X" -> "macos"
-        osName.startsWith("Windows") -> "windows"
-        else -> error("Unable to determine CI Host for OS: $osName. CI_HOST env can be set instead.")
-    }
-
-    // Check for non-existent, unaccounted for, or double-counted targets
-    val allTargets = kotlin.targets.map { it.name }.sorted()
-    val groupedTargets = hostTargets.values.flatten().map { it.name }.sorted()
-    check(groupedTargets == allTargets) {
-        "Bad host target grouping.\n\tExpected: $allTargets\n\tActual:   $groupedTargets"
-    }
-
-    hostTargets[host]!!.asSequence()
-}
-
-tasks.create("ciTest") {
-    ciHostTargets
-        .filterIsInstance<KotlinTargetWithTests<*, *>>()
-        .map { target -> "${target.name}Test" }
-        .forEach { targetTest ->
-            dependsOn(targetTest)
-        }
-}
-
-tasks.create("ciPublish") {
-    ciHostTargets
-        .map { it.name.uppercaseFirstChar() }
-        .map { if (it == "Metadata") "KotlinMultiplatform" else it }
-        .map { target -> "publish${target}PublicationToMavenRepository" }
-        .forEach { publishTarget ->
-            dependsOn(publishTarget)
-        }
-}
-
 tasks.withType<DokkaTask>().configureEach {
-    dokkaSourceSets.configureEach {
-        reportUndocumented = true
-        failOnWarning = true
-
-        val releaseVersionRef = version.toString()
-            .takeIf { version -> version.matches(Regex("""\d+\.\d+\.\d+""")) }
-            ?.let { version -> "v$version" }
-
-        if (releaseVersionRef != null) {
-            sourceLink {
-                localDirectory = projectDir.resolve("src")
-                remoteUrl.set(URL("https://github.com/BenWoodworth/Parameterize/tree/$releaseVersionRef/src"))
-                remoteLineSuffix = "#L"
-            }
-        }
-    }
-
     doLast {
         layout.buildDirectory.asFileTree.asSequence()
             .filter { it.isFile && it.extension == "html" }
