d3ecfcb6da75b9d0b98e5f400558f64d844ed0ab
Ben Woodworth <ben@benwoodworth.com>
2024-09-23 15:30:22 -0400

    Break up build script into convention plugins
    The library will be split up into modules, so this makes parts of the
    build configuration easy to reuse.

---
+++ buildSrc/src/main/kotlin/kotlin-multiplatform-conventions.gradle.kts
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2024 Ben Woodworth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+// https://github.com/gradle/gradle/issues/15383#issuecomment-1216098405
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+plugins {
+    kotlin("multiplatform")
+}
+
+@OptIn(ExperimentalWasmDsl::class)
+kotlin {
+    explicitApi()
+
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+        freeCompilerArgs.add("-Xexpect-actual-classes")
+    }
+
+    jvm {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js {
+        browser()
+        nodejs()
+    }
+
+    linuxX64()
+    linuxArm64()
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    iosArm64()
+    watchosDeviceArm64()
+    mingwX64()
+
+    wasmJs {
+        browser()
+        nodejs()
+    }
+    wasmWasi {
+        nodejs()
+    }
+
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.contracts.ExperimentalContracts")
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.findLibrary("kotlin-test").get())
+            }
+        }
+    }
+}
