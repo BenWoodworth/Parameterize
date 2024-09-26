754b0e355fd36afa98955339326254c54eae6647
Ben Woodworth <ben@benwoodworth.com>
2024-09-25 20:10:51 -0400

    Move project to parameterize-core
    Follows the convention used by the first party Kotlin libraries, which
    will make the core module's functionality clearer among other modules
    being added.
    
    Dokka has also been configured to publish multi-module documentation.

---
+++ parameterize-core/build.gradle.kts
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Ben Woodworth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+    id("kotlin-multiplatform-conventions")
+    id("dokka-conventions")
+    id("binary-compatibility-validator-conventions")
+    id("publishing-conventions")
+    id("ci-conventions")
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.opentest4j)
+            }
+        }
+    }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    doLast {
+        layout.buildDirectory.asFileTree.asSequence()
+            .filter { it.isFile && it.extension == "html" }
+            .forEach { file ->
+                file.readText()
+                    // Remove "ParameterizeConfiguration." prefix from link text for *Scope classes
+                    .replace(Regex("""(?<=>)ParameterizeConfiguration\.(?=\w+Scope</a>)"""), "")
+                    .let { file.writeText(it) }
+            }
+    }
+}
