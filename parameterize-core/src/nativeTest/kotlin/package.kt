b74f5e36f98870ffad3947a7bb3c3f6cbf9965fd
Ben Woodworth <ben@benwoodworth.com>
2024-10-07 21:29:52 -0400

    Move TestUtils.kt into the `test` package

--- parameterize-core/src/nativeTest/kotlin/package.kt
+++ parameterize-core/src/nativeTest/kotlin/package.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Ben Woodworth
+ * Copyright 2025 Ben Woodworth
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,3 @@
  */
 
 package com.benwoodworth.parameterize
-
-actual val Throwable.stackTraceLines: List<String>
-    get() = stackTrace.map { "at $it" }
