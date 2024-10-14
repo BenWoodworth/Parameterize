e2aa842586ba85fb18d4b42df70fa480c93469c3
Ben Woodworth <ben@benwoodworth.com>
2024-10-14 18:08:06 -0400

    WIP Improve how empty stack traces are implemented
    TODO: Check over
    
    Instead of clearing the stack traces after `Throwable`s are created,
    prevent the stack traces from being created in the first place. This
    saves computation since now stack traces are avoided all together on
    supported platforms.

--- parameterize-core/src/jsMain/kotlin/ParameterizeFailedError.js.kt
+++ parameterize-core/src/jsMain/kotlin/ParameterizeFailedError.js.kt
@@ -16,13 +16,28 @@
 
 package com.benwoodworth.parameterize
 
-public actual class ParameterizeFailedError internal actual constructor(
+public actual class ParameterizeFailedError private constructor(
     internal actual val recordedFailures: List<ParameterizeFailure>,
     internal actual val successCount: Long,
     internal actual val failureCount: Long,
     internal actual val completedEarly: Boolean
 ) : AssertionError() {
-    public actual companion object;
+    public actual companion object {
+        internal actual operator fun invoke(
+            recordedFailures: List<ParameterizeFailure>,
+            successCount: Long,
+            failureCount: Long,
+            completedEarly: Boolean
+        ): ParameterizeFailedError {
+            return if (commonShouldCaptureStackTrace(recordedFailures)) {
+                ParameterizeFailedError(recordedFailures, successCount, failureCount, completedEarly)
+            } else {
+                createThrowableSubclassWithoutStack {
+                    ParameterizeFailedError(recordedFailures, successCount, failureCount, completedEarly)
+                }
+            }
+        }
+    }
 
     init {
         commonInit()
@@ -32,8 +47,20 @@ public actual class ParameterizeFailedError internal actual constructor(
         get() = commonMessage
 }
 
-internal actual fun Throwable.clearStackTrace() {
-    // The `stack` property is non-standard, but supported on all major browsers/servers:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/stack#browser_compatibility
-    this.asDynamic().stack = null
+internal actual class Failure private constructor(
+    actual val failure: ParameterizeFailure
+) : AssertionError() {
+    actual companion object {
+        actual operator fun invoke(failure: ParameterizeFailure): Failure {
+            return createThrowableSubclassWithoutStack {
+                Failure(failure)
+            }
+        }
+    }
+
+    actual override val message: String
+        get() = commonMessage
+
+    actual override val cause: Throwable
+        get() = commonCause
 }
